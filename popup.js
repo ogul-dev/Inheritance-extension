337b25a0d1e4ba8252288ad5e0ce10c3418e1470
ogul-dev <70044177+ogul-dev@users.noreply.github.com>
2020-08-22 22:35:16 +1000

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,1 +1,21 @@
 //Popup.js
+//Output for startGame button
+function startGame(){
+  document.getElementsByTagName('body')[0].innerHTML = '';
+}
+// Add event listeners once the DOM has fully loaded by listening for the
+// `DOMContentLoaded` event on the document, and adding your listeners to
+// specific elements when it triggers.
+  document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector("#startButtonImage").addEventListener('click', startGame);
+  document.querySelector("#infoButtonImage").addEventListener('click', openInfo);
+});
+
+//Output for openInfo button
+function openInfo(){
+  document.getElementsByTagName('body')[0].innerHTML = '';
+  function show_image(src, alt) {
+    var img = document.createElement("img");
+    img.src = "info_screen.png";
+  }
+}
