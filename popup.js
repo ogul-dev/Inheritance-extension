4b0858dbc09965e2aeb7efa0112610bbe3c7f544
ogul-dev <70044177+ogul-dev@users.noreply.github.com>
2020-08-23 13:41:39 +1000

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,7 +1,16 @@
 //Popup.js
+//Shows the Opening Lobby
+function showLobby(src, alt) {
+  var img = document.createElement("img");
+  img.src = "opening_lobby.png";
+  img.alt = 'Opening Lobby';
+
+  document.body.appendChild(img);
+}
 //Output for startGame button
 function startGame(){
   document.getElementsByTagName('body')[0].innerHTML = '';
+  showLobby();
 }
 // Add event listeners once the DOM has fully loaded by listening for the
 // `DOMContentLoaded` event on the document, and adding your listeners to
@@ -10,12 +19,19 @@ function startGame(){
   document.querySelector("#startButtonImage").addEventListener('click', startGame);
   document.querySelector("#infoButtonImage").addEventListener('click', openInfo);
 });
+//Shows Info Screen Image
+function showImage(src, alt) {
+  var img = document.createElement("img");
+  img.src = "info_screen.png";
+  img.alt = 'Information Screen';
 
+  document.body.appendChild(img);
+}
 //Output for openInfo button
-function openInfo(){
+function openInfo() {
   document.getElementsByTagName('body')[0].innerHTML = '';
-  function show_image(src, alt) {
-    var img = document.createElement("img");
-    img.src = "info_screen.png";
-  }
+  showImage();
+}
+function openWindow() {
+  console.log("looks right")
 }
